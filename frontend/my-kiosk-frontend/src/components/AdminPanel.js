3f9ae64009e1c168041303c48135bcebe5dbca45
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2024-01-20 21:59:59 +0100

    dodanie nawigacji do panelu

--- frontend/my-kiosk-frontend/src/components/AdminPanel.js
+++ frontend/my-kiosk-frontend/src/components/AdminPanel.js
@@ -1,23 +1,36 @@
 // src/components/AdminPanel.js
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import { AdminContext } from '../context/AdminContext';
 import CategoriesManager from './CategoriesManager';
-import ProductsManager  from "./ProductsManager";
+import ProductsManager from "./ProductsManager";
 
 const AdminPanel = () => {
-    const { state } = useContext(AdminContext);
+    const { state, dispatch } = useContext(AdminContext);
+    const navigate = useNavigate(); // Initialize useNavigate
+
+    const handleLogout = () => {
+        dispatch({ type: 'LOGOUT' });
+        navigate('/admin'); // Redirect to the login page
+    };
 
     if (!state.isAdminAuthenticated) {
-        return <p>Access Denied</p>;
+        // If not authenticated, redirect to the login page with a message
+        return (
+            <div>
+                <p>Access Denied. You are not authorized to view this page.</p>
+                <p>Please <button onClick={() => navigate('/admin')}>click here</button> to login.</p>
+            </div>
+        );
     }
 
     return (
         <div className="admin-panel">
             <h1 className="admin-title">Admin Panel</h1>
+            <button onClick={handleLogout} className="delete">Logout</button>
             <CategoriesManager />
             <ProductsManager />
         </div>
-
     );
 };
 
